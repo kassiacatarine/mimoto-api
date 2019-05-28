07df2267c5b6befd140564b3e0aaca5e42fe3105
Kassia Catarine <kassia_catarine.15@hotmail.com>
2019-05-28 15:31:26 -0300

    Adding cors reference
    Signed-off-by: Kassia Catarine <kassia_catarine.15@hotmail.com>

--- Mimoto.Api/Startup.cs
+++ Mimoto.Api/Startup.cs
@@ -31,6 +31,8 @@ namespace Mimoto.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             // Register the Swagger generator, defining 1 or more Swagger documents
@@ -82,6 +84,8 @@ namespace Mimoto.Api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseCors(builder =>
+                builder.WithOrigins("http://localhost:4200"));
 
             app.UseHttpsRedirection();
             app.UseMvc();
